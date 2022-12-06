b9003e5f70657e920caa600e9814aa81984591f9
hyunsang-ahn <happylovetkd@naver.com>
2022-12-07 06:46:37 +0900

    20221207

--- cache/cache.js
+++ cache/cache.js
@@ -1,83 +1,57 @@
 function solution(cacheSize, cities) {
     let arr = []
-    // for (let i = 0; i < cacheSize; i++) {
-    //     arr.push(0)
-    // }
-    // console.log(arr)
-    if (cacheSize === 0) {
-        return cities.length * 5;
-    }
     let cnt = 0
-    for (city of cities) {
-        if (arr.length !== cacheSize) {
-            if (arr.length === 0) {
-                arr.push(city.toLowerCase())
-                cnt = cnt + 5
-
-            } else if (arr[0] === city) {
-                //첫번째가 같다면 굳이 추가 안해줘도 된다.
-                cnt = cnt + 1
 
 
-            } else {
+    if (cacheSize === 0) {
+        //캐시 사이즈가 0일때는 그대로 5를 곱해서 리턴한다.
+        return cities.length * 5
+    } else {
+        //0이 아닌경우에서
 
-                if (arr.includes(city.toLowerCase())) {
-                    const idx = arr.findIndex((el) => el === city.toLowerCase())
+        for (city of cities) {
+            if (arr.includes(city.toLowerCase())) {
+                //arr 안에 city가 있는경우...
+                console.log('city=================', city)
+                const idx = arr.indexOf(city.toLowerCase())
+                if (idx === 0) {
+                    //0이면 아무것도 할일잉 없다...
+                    //캐시 히트 
+                    cnt = cnt + 1
+                } else {
                     arr.splice(idx, 1)
                     arr.unshift(city.toLowerCase())
-
+                    //기존에 있던것을 삭제하고 가장 앞으로 이동시킨다.
+                    //캐시 히트 
                     cnt = cnt + 1
+                }
 
-                } else {
-
+            } else {
+                //arr 안에 city가 없는경우
+                //무조건 넣어야 하는대
+                //이때 캐쉬 사이즈의 한계를 설정한다.
+                if (arr.length < cacheSize) {
+                    //캐쉬사이즈보다 작으면 일단 추가
                     arr.unshift(city.toLowerCase())
+                    //캐시 미스 
+                    cnt = cnt + 5
+                } else {
+                    //캐시시아즈보다 크거나 같으면
+                    //가장 뒤에거가 삭제되고.. 
+                    //추가된다.
+                    arr.pop()
+                    arr.unshift(city.toLowerCase())
+                    //캐시 미스 
                     cnt = cnt + 5
                 }
-            }
-        } else {
-
-
-            //cache size이라면..
-            if (arr.includes(city.toLowerCase())) {
-                //캐시에 저장되어 있다면
-                //저장된 놈을 제일 앞으로 이동시킨다.
-                // let idx = arr.indexOf(city.toLowerCase())
-                // console.log('idx=============', idx)
-                // let new_arr;
-                // if (idx !== 0) {
-                //     if (idx === arr.length) {
-                //         //리스트 마지막 요소라면 
-                //         let temp_arr_1 = arr.splice(0, idx)
-                //         new_arr = [...temp_arr_1,]
-
-                //     } else {
-
-                //         let temp_arr_1 = arr.splice(0, idx)
-                //         let temp_arr_2 = arr.splice(idx, arr.length)
-                //         new_arr = [...temp_arr_1, ...temp_arr_2]
-                //     }
-                // } else {
-                //     new_arr = arr
-                // }
-                // new_arr = arr
-                // new_arr.pop()
-                // new_arr.unshift(city.toLowerCase())
-                // arr = new_arr
-                // cnt = cnt + 1
-                // console.log(city.toLowerCase())
-                arr.pop(0)
-                arr.unshift(city.toLowerCase())
-                cnt = cnt + 1
-            } else {
-                arr.pop(0)
-                arr.unshift(city.toLowerCase())
-                cnt = cnt + 5
 
             }
+            //포문 한번 돌때마다 arr 확인
+            console.log('arr=================', arr)
+            console.log('cnt=================', cnt)
         }
-
-
     }
+
 
     return cnt;
 }
