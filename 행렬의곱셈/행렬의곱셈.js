081200c598a63124d0740d681258620f3aec80fa
hyunsang-ahn <happylovetkd@naver.com>
2022-12-19 06:46:07 +0900

    20221219

--- 행렬의곱셈/행렬의곱셈.js
+++ 행렬의곱셈/행렬의곱셈.js
@@ -1,4 +1,18 @@
 function solution(arr1, arr2) {
-    var answer = [[]];
-    return answer;
-}
+    let res_arr = []
+    for (let i = 0; i < arr1.length; i++) {
+      let temp = []
+      for (let j = 0; j < arr2[0].length; j++) {
+        console.log(j)
+        let cnt = 0
+        for (let k = 0; k < arr2.length; k++) {
+          let temp_num = arr1[i][k] * arr2[k][j]
+          cnt += temp_num
+        }
+        temp.push(cnt)
+      }
+      res_arr.push(temp)
+    }
+    return res_arr
+  }
+  
