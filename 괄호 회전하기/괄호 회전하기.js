e7092046976192c94535223cce4ccb44d95974c5
hyunsang-ahn <happylovetkd@naver.com>
2022-12-26 06:56:45 +0900

    20221226

--- 괄호 회전하기/괄호 회전하기.js
+++ 괄호 회전하기/괄호 회전하기.js
@@ -3,6 +3,7 @@ function solution(s) {
   let length = s.length
   let cnt = 0
 
+  //이거..스택으로 풀어야함..
   for (let i = 0; i < length; i++) {
     console.log(i)
     let new_str
@@ -15,49 +16,58 @@ function solution(s) {
       new_str = s.slice(i, length) + s.slice(0, i)
     }
     let new_arr = new_str.split('')
-    while (true) {
-        if(new_arr.length === 0){
-            cnt++
-            break
+
+    if (new_str[0] === ']' || new_str[0] === '}' || new_str[0] === ')') {
+      //닫는 괄호들로 시작하면 절대 맞지 않는다...
+    } else {
+      let new_stack_arr = []
+
+      new_arr.map((c, i) => {
+        if (new_stack_arr.length === 0) {
+          new_stack_arr.push(c)
+        } else {
+          if (c === ']' || c === '}' || c === ')') {
+            //닫는 괄호들로 시작하면 앞에꺼랑 비교하고
+            if (c === '}') {
+              if (new_stack_arr[new_stack_arr.length - 1] === '{') {
+                //삭제한다.
+                new_stack_arr.pop()
+              } else {
+                new_stack_arr.push(c)
+              }
+            } else if (c === ']') {
+              if (new_stack_arr[new_stack_arr.length - 1] === '[') {
+                //삭제한다.
+                new_stack_arr.pop()
+              } else {
+                new_stack_arr.push(c)
+              }
+            } else if (c === ')') {
+              if (new_stack_arr[new_stack_arr.length - 1] === '(') {
+                //삭제한다.
+                new_stack_arr.pop()
+              } else {
+                new_stack_arr.push(c)
+              }
+            }
+          } else {
+            //여는 괄호면 무조건 넣는다.
+            new_stack_arr.push(c)
+          }
         }
+      })
 
-
-      if (new_str[0] === ']' || new_str[0] === '}' || new_str[0] === ')') {
-        //닫는 괄호들로 시작하면 절대 맞지 않는다...
-        break
-      } else {
-        console.log(new_arr)
-        if (new_arr[0] === '[') {
-          //여는 태그라면 닫는태그가 바로 옆에 있어야함.
-          if (new_arr[1] === ']') {
-            new_arr = new_arr.slice(2, new_arr.length)
-
-          } else {
-            break
-          }
-        } else if (new_arr[0] === '{') {
-          //여는 태그라면 닫는태그가 바로 옆에 있어야함.
-          if (new_arr[1] === '}') {
-            new_arr = new_arr.slice(2, new_arr.length)
-
-          } else {
-            break
-          }
-        } else if (new_arr[0] === '(') {
-          //여는 태그라면 닫는태그가 바로 옆에 있어야함.
-          if (new_arr[1] === ')') {
-            new_arr = new_arr.slice(2, new_arr.length)
-
-          } else {
-            break
-          }
-        }
+      if (new_stack_arr.length === 0) {
+        //빈 배열이라면 cnt 늘린다.
+        cnt = cnt + 1
       }
     }
   }
+
+
+  console.log('cnt===============', cnt)
+
   return cnt
 }
 
-
-
-solution("}]()[{")
+solution('}]()[{')
