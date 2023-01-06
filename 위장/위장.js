61dadd3a9cbe4f72c34ee114a7903212321b621b
hyunsang-ahn <happylovetkd@naver.com>
2023-01-07 07:11:40 +0900

    20230107

---
+++ 위장/위장.js
@@ -0,0 +1,19 @@
+function solution(clothes) {
+    let clothes_type = []
+    
+    clothes.map((c)=>{
+        if(clothes_type.includes(c[1])){
+            
+        }else{
+            clothes_type.push(c[1])
+        }
+    })
+    
+    console.log(clothes_type)
+clothes_type.map((d)=>{
+    
+})
+    
+    var answer = 0;
+    return answer;
+}
