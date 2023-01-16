0b60c5c4cf0e24efc6ed621dd82d56b37e615ae4
hyunsang-ahn <happylovetkd@naver.com>
2023-01-17 06:57:31 +0900

    20230117

---
+++ 배열 자르기/배열 자르기.js
@@ -0,0 +1,25 @@
+function solution(n, left, right) {
+
+    for(let i=0; i <n; i++){
+        console.log(i)
+
+
+        if(i === 0){
+            let temp_arr = []
+            for(let k=1; k <=n; k++){
+                temp_arr.push(k)
+
+            }
+            console.log(temp_arr)
+        }else{
+            
+        }
+    }
+    var answer = [];
+    return answer;
+}
+
+
+
+
+solution(3,2,5)
